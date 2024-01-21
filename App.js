59c46396f82c358ece039b6e99c04d21cf2bd690
Aman Singh <137636775+code-with-aman111@users.noreply.github.com>
2024-01-21 05:28:14 -0800

    App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import Details from './Details/Details'
+import Main_c from './Main_c/Main'
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Details/>
+     <Main_c/>
+    </div>
+  )
+}
+
+export default App
